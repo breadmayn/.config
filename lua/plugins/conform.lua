2968cf74c0be7a5c03213ac39e89345bca62df2d
Brandon Mach <brandonmachh@gmail.com>
2026-08-16 16:41:36 -0700

    finish initial conform configuration

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,5 +1,5 @@
 -- [[
--- 
+--
 -- This file will import conform.nvim and include our configurations of the plugin
 --
 -- Conform is a plugin used to hook in formatters for ease of use during development
@@ -7,22 +7,26 @@
 -- ]]
 
 return {
-    'stevearc/conform.nvim',
-    opts = {
-        formatters_by_ft = {
-            lua = { 'stylua' },
-        },
-    },
+	"stevearc/conform.nvim",
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+		},
+	},
 
-    -- keymaps for conform.nvim
-    keys = {
-        {
-            '<leader>f',
-            function()
-                require('conform').format()
-            end,
-            desc = '[F]ormat current buffer',
-        },
-    },
+	-- keymaps for conform.nvim
+	keys = {
+
+		{
+			"<leader>f",
+			function()
+				require("conform").format()
+			end,
+			desc = "[F]ormat current buffer",
+		},
+	},
+
+	-- adding cmd to resolve if conform isn't already loaded
+	-- note: calling ConformInfo will fully load conform.nvim
+	cmd = { 'ConformInfo' },
 }
-
