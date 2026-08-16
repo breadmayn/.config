d325a4424b4bcc7d05e44df825ff151e3b823a12
Brandon Mach <brandonmachh@gmail.com>
2026-08-16 16:46:48 -0700

    my initial treesitter configuration

--- after/ftplugin/lua.lua
+++ after/ftplugin/lua.lua
@@ -3,13 +3,19 @@
 -- after/ftplugin/lua.lua
 --
 -- This file handles the logic for when we open .lua files.
--- Specifically, this file acts as a fallback when .editorconfig is not given from the project to manage how the editor should act
--- 
+--
+-- Table of Contents:
+-- 1. Manage fallback settings when no .editorconfig exists in project
+-- 2. Enable treesitter highlighting/parsing for
 -- ]]
 
--- attempt to match stylua formatting by default when no project specific policy is stated
+-- 1. fallback editor settings
+	-- attempt to match stylua formatting by default when no project specific policy is stated
 vim.opt_local.expandtab = false
 vim.opt_local.shiftwidth = 4
 vim.opt_local.tabstop = 4
 vim.opt_local.softtabstop = 4
+
+-- 2. enable treesitter
+vim.treesitter.start()
 
