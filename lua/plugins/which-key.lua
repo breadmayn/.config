12e6510f9d092f6bfb60eebb024521e85a90837e
Brandon Mach <brandonmachh@gmail.com>
2026-08-16 21:13:42 -0700

    add which-key configuration

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,57 @@
+-- [[
+--
+-- lua/plugins/which-key.lua
+--
+-- This file will import which-key.nvim and include our current configurations of the plugin
+--
+-- Which-key is a plugin which helps search through keymappings that are defined within the NVIM system
+--
+-- ]]
+
+return {
+	"folke/which-key.nvim",
+
+	-- set the event we would like which-key to load with
+	event = "VimEnter",
+
+	opts = {
+		-- set the delay between key press and opening which key
+		delay = 0, -- milliseconds
+		icons = {
+			-- set icon mappings to true if you have a Nerd Font
+			mappings = vim.g.have_nerd_font,
+			-- If you are using a Nerd Font: set icons.keys to an empty table which will use the
+			-- default which-key.nvim defined Nerd Font icons, otherwise define a string table
+			keys = vim.g.have_nerd_font and {} or {
+				Up = "<Up> ",
+				Down = "<Down> ",
+				Left = "<Left> ",
+				Right = "<Right> ",
+				C = "<C-…> ",
+				M = "<M-…> ",
+				D = "<D-…> ",
+				S = "<S-…> ",
+				CR = "<CR> ",
+				Esc = "<Esc> ",
+				ScrollWheelDown = "<ScrollWheelDown> ",
+				ScrollWheelUp = "<ScrollWheelUp> ",
+				NL = "<NL> ",
+				BS = "<BS> ",
+				Space = "<Space> ",
+				Tab = "<Tab> ",
+				F1 = "<F1>",
+				F2 = "<F2>",
+				F3 = "<F3>",
+				F4 = "<F4>",
+				F5 = "<F5>",
+				F6 = "<F6>",
+				F7 = "<F7>",
+				F8 = "<F8>",
+				F9 = "<F9>",
+				F10 = "<F10>",
+				F11 = "<F11>",
+				F12 = "<F12>",
+			},
+		},
+	},
+}
