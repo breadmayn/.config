7b2969571dd18c55a54a736e1c02bfc69d2380d6
Brandon Mach <brandonmachh@gmail.com>
2026-08-18 14:53:07 -0700

    fix typo in smart file explorer toggle keymap

--- lua/plugins/neotree/keymaps.lua
+++ lua/plugins/neotree/keymaps.lua
@@ -11,10 +11,11 @@ return {
 	{
 		'<leader>e',
 		function()
+			-- this is a smart toggle where if we have opened a file not in the cwd then we won't follow just reveal the file explorer
 			local name = vim.api.nvim_buf_get_name(0)
 			local inside = name ~= '' and vim.startswith(name, vim.fn.getcwd() .. '/')
 
-			vim.cmd('Neotree toggle' .. (inside and 'reveal' or ''))
+			vim.cmd('Neotree toggle' .. (inside and ' reveal' or ''))
 		end,
 		desc = 'toggle file explorer'
 	},
