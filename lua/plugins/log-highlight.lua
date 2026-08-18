d131387692af69d4af71220b366c928d8e468bfe
Brandon Mach <brandonmachh@gmail.com>
2026-08-17 21:26:42 -0700

    add log-highlight plugin

---
+++ lua/plugins/log-highlight.lua
@@ -0,0 +1,43 @@
+-- [[
+--
+-- lua/plugins/log-highlight.lua
+--
+-- This file will import log-highlight.nvim and include our configurations of the plugin
+--
+-- Log-highlight is a plugin that provides a robust log viewer
+--
+-- ]]
+
+return {
+	"fei6409/log-highlight.nvim",
+
+	-- lazily load this plugin once we open a .log file
+	ft = {
+		"log",
+	},
+
+	opts = {
+		extension = "log", -- default
+
+		filename = {
+			"syslog", -- example from the README
+		},
+
+		pattern = {
+			"%/var%/log%/.*",
+			"console%-ramoops.*",
+			"log.*%.txt",
+			"logcat.*",
+		},
+
+		-- denote keyword that should get highlighed
+		keyword = {
+			error = { "ERROR", "FATAL", "PANIC", "CRITICAL" },
+			warning = { "WARN", "WARNING" },
+			info = { "INFO", "NOTICE" },
+			debug = { "DEBUG", "TRACE" },
+			pass = { "PASS", "OK", "SUCCESS" },
+		},
+	},
+}
+
