d7214ebe26c1604d290363e68b906683853b595a
Brandon Mach <brandonmachh@gmail.com>
2026-08-17 21:29:16 -0700

    update for nvim specific overseer templates

--- lua/plugins/overseer/util.lua
+++ lua/plugins/overseer/util.lua
@@ -13,7 +13,7 @@ local M = {}
 -- ]]
 function M.get_project_scripts()
 	local root = vim.fn.getcwd() -- note that the project root dir is where nvim will be invoked
-	local scripts_dir = vim.fs.joinpath(root, 'scripts')
+	local scripts_dir = vim.fs.joinpath(root, '.nvim', 'scripts')
 
 	-- early bail if there is no scripts dir
 	if vim.fn.isdirectory(scripts_dir) ~= 1 then
