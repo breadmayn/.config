7863c794f5f5de152d31806a6c57c55799c27d7e
Brandon Mach <brandonmachh@gmail.com>
2026-08-24 22:26:38 -0700

    update neotree description

--- lua/plugins/neotree/keymaps.lua
+++ lua/plugins/neotree/keymaps.lua
@@ -15,9 +15,9 @@ return {
 			local name = vim.api.nvim_buf_get_name(0)
 			local inside = name ~= '' and vim.startswith(name, vim.fn.getcwd() .. '/')
 
-			vim.cmd('Neotree toggle' .. (inside and ' reveal' or ''))
+			vim.cmd('neotree toggle' .. (inside and ' reveal' or ''))
 		end,
-		desc = 'toggle file explorer'
+		desc = 'toggle file [e]xplorer'
 	},
 
 }
