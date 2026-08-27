784b68941aced6cd73f594b8957e353a6cb625e2
Brandon Mach <brandonmachh@gmail.com>
2026-08-26 19:58:34 -0700

    add new window management core keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -14,6 +14,7 @@
 --		3. Extra transitions into normal mode
 --			a. Terminal mode exit
 --		4. Window management (NVIM native)
+			-- see `:help wincmd`
 --		5. Quickfix list
 --
 -- ]]
@@ -37,19 +38,83 @@ vim.keymap.set('i', '<M-f>', '<C-Right>', { desc = 'macOS style navigation to th
 vim.keymap.set('i', '<M-BS>', '<C-w>', { desc = 'macOS style backspace to clear entire word (behind current cursor placement)' })
 
 -- 3. Extra transitions out of modes into normal mode
-vim.keymap.set({ 'i', 't' }, 'jjk', [[<C-\><C-n>]], { desc = 'Exit terminal mode and return to normal mode' })
-vim.keymap.set({ 'i', 't' }, 'kkj', [[<C-\><C-n>]], { desc = 'Exit terminal mode and return to normal mode' })
+vim.keymap.set({ 'i', 't' }, '<C-c>', [[<C-\><C-n>]], { desc = 'Exit current mode and return to normal mode' })
+vim.keymap.set({ 'i', 't' }, '<C-c>', [[<C-\><C-n>]], { desc = 'Exit current mode and return to normal mode' })
 
 -- 3a. Terminal mode exit
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode and return to normal mode' })
 
 -- 4. Window management
-    -- see `:help wincmd`
-    -- <C-w> is short hand for wincmd I think
+	-- <C-w> is short hand for wincmd I think
 vim.keymap.set({ 'n', 't' }, '<C-h>', [[<cmd>wincmd h<CR>]], { desc = 'Move focus to the left window' })
 vim.keymap.set({ 'n', 't' }, '<C-l>', [[<cmd>wincmd l<CR>]], { desc = 'Move focus to the right window' })
 vim.keymap.set({ 'n', 't' }, '<C-j>', [[<cmd>wincmd j<CR>]], { desc = 'Move focus to the upper window' })
 vim.keymap.set({ 'n', 't' }, '<C-k>', [[<cmd>wincmd k<CR>]], { desc = 'Move focus to the lower window' })
+
+-- [[
+-- This function is a utility that enables the mental model of moving the divider rather than inc/dec window size
+--
+-- This checks if there is a window in the direction we pointed towards
+-- ]]
+local function has_neighbor(direction)
+	return vim.fn.winnr(direction) ~= vim.fn.winnr()
+end
+
+-- [[
+-- This function is a wrapper which handles all of the resizing keymaps that we will set here
+-- ]]
+local function resize(direction, amount)
+	amount = amount or 2
+
+	if direction == 'left' then
+		if has_neighbor('h') then
+			vim.cmd('vertical resize +' .. amount)
+		elseif has_neighbor('l') then
+			vim.cmd('vertical resize -' .. amount)
+		end
+	elseif direction == 'right' then
+		if has_neighbor('l') then
+			vim.cmd('vertical resize +' .. amount)
+		elseif has_neighbor('h') then
+			vim.cmd('vertical resize -' .. amount)
+		end
+	elseif direction == 'up' then
+		if has_neighbor('k') then
+			vim.cmd('resize +' .. amount)
+		elseif has_neighbor('j') then
+			vim.cmd('resize -' .. amount)
+		end
+	elseif direction == 'down' then
+		if has_neighbor('j') then
+			vim.cmd('resize +' .. amount)
+		elseif has_neighbor('k') then
+			vim.cmd('resize -' .. amount)
+		end
+	end
+end
+
+vim.keymap.set('n', '<A-h>', function()
+	resize('left')
+end, { desc = 'move window divider left' })
+
+vim.keymap.set('n', '<A-l>', function()
+	resize('right')
+end, { desc = 'move window divider right' })
+
+vim.keymap.set('n', '<A-j>', function()
+	resize('down')
+end, { desc = 'move window divider down' })
+
+vim.keymap.set('n', '<A-k>', function()
+	resize('up')
+end, { desc = 'move window divider up' })
+
+-- [[
+-- some default wincmd keymaps
+--	Split horizontally: <C-w>s
+--	Split vertically: <C-w>v
+--	Close current window: <C-w>c
+-- ]]
 
 -- 5. Quickfix list
 vim.keymap.set('n', ']q', '<cmd>cnext<CR>', { desc = 'Next quickfix item' })
